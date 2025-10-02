d0d77f148a73257eb0ab328648416e7a201f4e71
thorpeto <tpetersson@web.de>
2025-10-02 13:43:44 +0000

    AD Ver 0.0.3

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,14 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2826152307009467"
           crossOrigin="anonymous"
         ></script>
+        {/* Google Analytics Tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GCCKR3MDHZ"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-GCCKR3MDHZ');
+        ` }} />
       </head>
       <body className={inter.className}>
         {children}
